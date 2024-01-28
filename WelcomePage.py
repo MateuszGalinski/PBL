3ebe739f1fadf982942b6945c36a6993c36ffff4
MateuszGalinski <mateusz.galinski020@gmail.com>
2024-01-28 22:05:44 +0100

    white sidebar

--- WelcomePage.py
+++ WelcomePage.py
@@ -1,4 +1,13 @@
 import streamlit as st
 
+st.markdown("""
+<style>
+    [data-testid=stSidebarNavLink] span {
+        color: #ffffff !important;
+    }
+</style>
+""", unsafe_allow_html=True)
+
+
 st.markdown("<div style='text-align: center;'><h1 style='color: black;'>Welcome to beta version of this map!</h1></div>", unsafe_allow_html=True)
 st.markdown("<div style='text-align: center;'><h4 style='color: black;'>Click arrow in left-upper corner or navigation bar to begin</h4></div>", unsafe_allow_html=True)
